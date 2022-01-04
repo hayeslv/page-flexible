52d7ee09b969c18ddd5a3a6f85ae3b3717a6dddf
lvhaizhou <417703682@qq.com>
2022-01-04 15:04:10 +0800

    style优化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-
 import debounce from 'lodash.debounce';
 
-const originWidth = 3840;
-const originHeight = 1080;
-
-const time = 100; // 防抖延迟时间
-
-export const getScale = () => {
+const getScale = (originWidth, originHeight) => {
   let width = originWidth; 
   const height = originHeight;
   const ww = window.innerWidth / width;
@@ -23,43 +17,47 @@ export const getScale = () => {
   return { rate, width, height };
 };
 
-const setScale = () => {
-  const { rate, width, height } = getScale();
-  // TODO 这种修改方式待优化，需要修改成setAttribute
-  document.documentElement.style = `width: 100vw; height: 100vh;`; // 固定html宽高
-  document.body.style = `width: ${originWidth * rate}px; height: 100%; overflow-y: hidden;`; // 确定body样式
-  
-  let style = `width: ${width}px; height: ${height}px; transform-origin: left top; overflow-y: hidden; pointer-events: none;`;
-  style += `transform: scale(${rate});`;
-  
+const setScale = (originWidth, originHeight, contentId) => {
+  const { rate, width, height } = getScale(originWidth, originHeight);
 
-  // TODO 代码侵入性太强，待优化
-  const id = document.getElementById('layer');
-  id.style = style;
-
+  document.documentElement.style.cssText = document.documentElement.style.cssText + `;width: 100vw; height: 100vh;`; // 固定html宽高
+  document.body.style.cssText = document.body.style.cssText + `;width: ${originWidth * rate}px; height: 100%; overflow-y: hidden; margin: 0;`; // 确定body样式
+  
+  // 内容区域
+  const content = document.getElementById(contentId);
+  if(!content) return;
+  const contentStyle = `
+    ;width: ${width}px;
+    height: ${height}px;
+    transform: scale(${rate});
+    transform-origin: left top; 
+    overflow-y: hidden; 
+    pointer-events: none; 
+    position: absolute;
+    left: 0;
+    top: 0;
+  `;
+  content.style.cssText = content.style.cssText + contentStyle;
   // 子元素解除css事件穿透
-  id.childNodes.forEach(node => node.style.pointerEvents = 'auto')
+  content.childNodes.forEach(node => node.style.pointerEvents = 'auto')
 }
 
-const debounceSetScale = debounce(setScale, time);
 
-debounceSetScale();
+/**
+ * 
+ * @param {*} originWidth ：设计稿宽度
+ * @param {*} originHeight ：设计稿高度
+ * @param {*} contentId ：内容区域id
+ * @param {*} time ：防抖时间
+ */
+export default function(originWidth = 1920, originHeight = 1080, contentId = 'layer', time = 100) {
+  const debounceSetScale = debounce(setScale, time);
+  debounceSetScale(originWidth, originHeight, contentId);
+  window.addEventListener('resize', () => debounceSetScale(originWidth, originHeight, contentId));
 
-window.addEventListener('resize', debounceSetScale);
-
-
-// 主动触发resize事件（让地图在当前宽高下重新绘制）
-setTimeout(() => {
-  var myEvent = new Event('resize');
-  window.dispatchEvent(myEvent);
-}, time);
-
-
-
-
-export const getSacleRate = () => {
-  const { rate } = getScale();
-  return rate;
-};
-
-
+  // 主动触发resize事件（让地图在当前宽高下重新绘制）
+  setTimeout(() => {
+    var myEvent = new Event('resize');
+    window.dispatchEvent(myEvent);
+  }, time);
+}
